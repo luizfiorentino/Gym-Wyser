49000aeea2b24d45264479bef91a8c4858180e27
luizfiorentino <lgfiorentino@gmail.com>
2026-08-25 16:42:35 +0200

    step 1 exercise selection toggle logic ready

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [trainSubmitted, setTrainSubmitted] = useState(false);
   const [exercises, setExercises] = useState([
     {
       group: "chest",
@@ -183,7 +184,6 @@ export default function Home() {
   };
 
   const addOrRemoveExercise = (exercise, group) => {
-    //console.log("addOrRemoveEx, exercise:", exercise, "Group:", group);
     setExercises(
       exercises.map((item) =>
         item.group === group.group
@@ -200,19 +200,66 @@ export default function Home() {
     );
   };
 
+  const submitTrain = () => {
+    if (trainSubmitted) {
+      setExercises(
+        exercises.map((item) => {
+          return { ...item, isSelected: false };
+        }),
+      );
+    }
+    setTrainSubmitted(!trainSubmitted);
+  };
+
   useEffect(() => {
-    console.log("useEffect, exercises:", exercises);
-  }, [exercises]);
+    //console.log("useEffect, exercises:", exercises);
+  }, [exercises, trainSubmitted]);
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Welcome to Gym-Wize
+            Welcome to Gym-Wizer
           </h1>
 
-          {exercises.length &&
+          {
+            <div>
+              <div>
+                {exercises.find((group) =>
+                  group.exercises?.find((exercise) => exercise.chosen),
+                )
+                  ? "Selected Exercises"
+                  : "Add exercises"}
+              </div>
+              {exercises.map((item, i) => (
+                <div key={i}>
+                  <h2>
+                    {item.exercises?.find((exercise) => exercise.chosen) &&
+                      item.group}
+                  </h2>
+                  <div>
+                    {" "}
+                    {item.exercises &&
+                      item.exercises.map((exercise) =>
+                        exercise.chosen ? (
+                          <ul
+                            onClick={() =>
+                              addOrRemoveExercise(exercise.name, item)
+                            }
+                          >
+                            {exercise.name} - remove
+                          </ul>
+                        ) : null,
+                      )}
+                  </div>
+                </div>
+              ))}
+            </div>
+          }
+
+          {!trainSubmitted &&
+            exercises.length &&
             exercises.map((group, i) => (
               <div key={i}>
                 <button onClick={() => displayGroupExercises(group.group)}>
@@ -222,7 +269,7 @@ export default function Home() {
                   group?.exercises?.map((item, j) => (
                     <div key={j}>
                       <ul onClick={() => addOrRemoveExercise(item.name, group)}>
-                        {item.name}
+                        {item.name} {!item.chosen ? "+" : "-"}
                       </ul>
                     </div>
                   ))}
@@ -231,21 +278,18 @@ export default function Home() {
         </div>
 
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Confirm train
-          </a>
+          <Image
+            className="dark:invert h-[14px] w-4"
+            src="/vercel.svg"
+            alt="Vercel logomark"
+            width={16}
+            height={14}
+          />
+          <div className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]">
+            <button onClick={() => submitTrain()}>
+              {!trainSubmitted ? "Confirm train" : "Return"}
+            </button>
+          </div>
 
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
